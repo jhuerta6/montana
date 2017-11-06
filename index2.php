d8ed288ba408ec5f2427c8d591e94446050cc755
ulises <ulimartinez96@gmail.com>
2017-11-06 10:29:43 -0700

    Done: fixed legend for point and line. Doing: meet with Imad, presentation. To-do: presentation, meet with Imad, clean up (visual and functional/logic)

--- index2.php
+++ index2.php
@@ -447,11 +447,11 @@ if(!isset($_SESSION['in']) OR !$_SESSION['in']){
                   var spawner = document.getElementById('legendSpawner');
                     var div = document.createElement('div');
                     div.innerHTML =
-                    "<img src='img/brightgreensquare.png' height='10px'/> Pavement has very good condition" +
-                    "<br> <img src='img/skybluesquare.png' height='10px'/> Pavement has good condition"+
-                    "<br> <img src='img/yellowsquare.png' height='10px'/> Pavement has fair condition"+
-                    "<br> <img src='img/orangesquare.png' height='10px'/> Pavement has poor condition"+
-                    "<br> <img src='img/redsquare.png' height='10px'/> Pavement has very poor condition";
+                    "<img src='img/brightgreensquare.png' height='10px'/> Pavement has <strong>very good</strong> condition" +
+                    "<br> <img src='img/skybluesquare.png' height='10px'/> Pavement has <strong>good</strong> condition"+
+                    "<br> <img src='img/yellowsquare.png' height='10px'/> Pavement has <strong>fair</strong> condition"+
+                    "<br> <img src='img/orangesquare.png' height='10px'/> Pavement has <strong>poor</strong> condition"+
+                    "<br> <img src='img/redsquare.png' height='10px'/> Pavement has <strong>very poor</strong> condition";
                     var newLegend = document.createElement('div');
                     newLegend = document.getElementById('legend');
                     document.getElementById('legend').style.visibility = "visible";
