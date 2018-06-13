6d194ac3d12be3045dbaa81fd8e9732e455a3aec
jhuerta6 <jorgehuertahu@gmail.com>
2018-06-12 19:03:25 -0600

    added new slides

--- corridors.php
+++ corridors.php
@@ -63,7 +63,7 @@ if(!isset($_SESSION['in_mpo']) OR !$_SESSION['in_mpo']){
         <img src="img/ctis_transparent_white_2017.png" style="width: 50px; height: 50px; float: right;">
     </h3>
     <h6 class="hidden-xs text-center" style="padding-left:35px;"><i style="color: white;">"</i><strong><i style="color:#FF8000;" class="text-center">CTIS </i></strong><i class="text-center" style="color:white;">is designated as a Member of National, Regional, and Tier 1 University Transportation Center."</i></h6>
-    <p class="hidden-xs text-right" style="color: white"> Version 1.5.6 (06/12/2018)</p>
+    <p class="hidden-xs text-right" style="color: white"> Version 1.5.7 (06/12/2018)</p>
 </nav>
 
 <div class="container panel panel-default toPDF">
@@ -759,16 +759,16 @@ if(!isset($_SESSION['in_mpo']) OR !$_SESSION['in_mpo']){
                         <!-- Wrapper for slides -->
                         <div class="carousel-inner" width="100%" role="listbox">
                             <div class="item active">
-                                <img class="center-block text-center" src="./slides/Slide1.PNG" alt="">
+                                <img class="center-block text-center" src="./slides/new/Slide1.PNG" alt="">
                             </div>
                             <div class="item">
-                                <img class="center-block text-center" src="./slides/Slide2.PNG" alt="">
+                                <img class="center-block text-center" src="./slides/new/Slide2.PNG" alt="">
                             </div>
                             <div class="item">
-                                <img class="center-block text-center" src="./slides/Slide3.PNG" alt="">
+                                <img class="center-block text-center" src="./slides/new/Slide3.PNG" alt="">
                             </div>
                             <div class="item">
-                                <img class="center-block text-center" src="./slides/Slide4.PNG" alt="">
+                                <img class="center-block text-center" src="./slides/new/Slide4.PNG" alt="">
                             </div>
                         </div>
 
