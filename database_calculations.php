5fd0fab078fa86d700616e683b279f77b33b2f97
seb1gonzalez <sebastian95gonzalez@gmail.com>
2019-01-09 14:25:35 -0700

    creating function for  query

--- database_calculations.php
+++ database_calculations.php
@@ -5,61 +5,58 @@
  * Date: 1/7/19
  * Time: 3:55 PM
  */
-
 ini_set('memory_limit', '-1');
 ini_set('max_execution_time', 30000); //300 seconds = 5 minutes
-
 //connection to utep database
 $conn = mysqli_connect('ctis.utep.edu', 'ctis', '19691963', 'mpo_test_jhuerta');
 
-//mysqli_close($conn);
-// calculating: NonSOV_e: [X08_COMMUTING.B08301e1] - [X08_COMMUTING.B08301e3]
- //cols a = B08301e1 , b = B08301e3
- //NonSOV_e = a - b
-$col_a = "b08301e1";
-$col_b = "b08301e3";
-// Start of multi query example- it works, all data is concatenated though.
-//$sql = "SELECT ".$col_a." FROM pm1;";
-//$sql .= "SELECT ".$col_b." FROM pm1;";
+//////////////////////////////////////---NonSOV_e begin--/////////////////////////////////////////////////
+//  calculating: NonSOV_e: [X08_COMMUTING.B08301e1] - [X08_COMMUTING.B08301e3]
+//  cols a = B08301e1 , b = B08301e3
+//  NonSOV_e = a - b
+$B08301e1 = "b08301e1";
+$B08301e3 = "b08301e3";
+$nonSOV_e = [];
+$data1 = getCol($conn,$B08301e1,"pm1");
+$data2 = getCol($conn,$B08301e3,"pm1");
+$arrlength = count($data1);
 
-// Execute multi query
-//if (mysqli_multi_query($conn,$sql))
-//{
-//    do
-//    {
-//        // Store first result set
-//        if ($result=mysqli_store_result($conn)) {
-//            // Fetch one and one row
-//            while ($row=mysqli_fetch_row($result))
-//            {
-//                printf("%s\n",$row[0]);
-//            }
-//            // Free result set
-//            mysqli_free_result($result);
-//        }
-//    }
-//    while (mysqli_next_result($conn));
-//}
-//
-//mysqli_close($conn);
-// end of multi query
+// loop through data and insert calculation into array
+// pseudo code: loop(arr[x] = data1[x] - data2[x];)
+echo "NonSOV_e";
+for($x = 0; $x < $arrlength; $x++) {
+    array_push($nonSOV_e,number_format((float)$data1[$x] - $data2[$x],6));
+    echo $nonSOV_e[$x]; // 'echo' for visualization & testing purposes
+    echo "<br>";
+}
+echo "<hr>";
+//////////////////////////////////////---End of NonSOV_e--/////////////////////////////////////////////////
 
-//single query approach
-//$query = "SELECT b08301e1 FROM pm1;";
-//$result = mysqli_query($conn, $query); // do the query, store in result
-//$arr_1 = array();
-//while($row = $result->fetch_array()){
-//    $arr_1[]= $row;
-//}
-//echo "<div class='container'>";
-//
-//foreach ($arr_1 as $key => $value){
-//    print_r($value[$col_a]."\n");
-//    echo "<br>";
-//}
-//echo "</div>";
+//////////////////////////////////////---NonSOV_m begin--/////////////////////////////////////////////////
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//implemented functions:
+/* 1. getCol(x,y,z): receives 3 inputs, outputs 1 array of type: $array = [];
+ *Precondition:
+ * @required (connection != False && colName exists in tableName)
+ */
 function getCol($conn,$colName,$tableName){
     $toReturn = [];
     $query = "SELECT $colName FROM $tableName;";
@@ -69,34 +66,11 @@ function getCol($conn,$colName,$tableName){
         $arr_1[]= $row;
     }
     foreach ($arr_1 as $key => $value){
-         array_push($toReturn,$value[$colName]);
-
+        array_push($toReturn,$value[$colName]);
     }
     return $toReturn;
 }
-$data1 = getCol($conn,$col_a,"pm1");
-$data2 = getCol($conn,$col_b,"pm1");
-$nonsov = [];
-$arrlength = count($data1);
-for($x = 0; $x < $arrlength; $x++) {
-    array_push($nonsov,number_format((float)$data1[$x] - $data2[$x],6));
-    echo $nonsov[$x];
-    echo "<br>";
-}
 
-////start of second pass
-//$query = "SELECT b08301e3 FROM pm1;";
-//$result = mysqli_query($conn, $query); // do the query, store in result
-//$arr_2 = array();
-//while($row = $result->fetch_array()){
-//    $arr_2[]= $row;
-//}
-//echo "<div class='container'><hr>";
-//
-//foreach ($arr_2 as $key => $value){
-//    print_r($value[$col_b]."\n");
-//    echo "<br>";
-//}
-//echo "</div>";
+// at the end, close connection
 mysqli_close($conn);
 ?>
