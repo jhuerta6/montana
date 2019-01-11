2b4b580757e0644a8906bc910818d21ae61c4062
seb1gonzalez <sebastian95gonzalez@gmail.com>
2019-01-11 14:29:56 -0700

    larry

--- database_calculations.php
+++ database_calculations.php
@@ -15,6 +15,8 @@ $dataset = array();
 // Dictionary to store results
 $lookup_result = array();
 
+$larry = getCol($conn,"pm1");
+var_dump($larry);
 
 
 
@@ -36,54 +38,54 @@ $lookup_result = array();
 //  calculating: NonSOV_e: [X08_COMMUTING.B08301e1] - [X08_COMMUTING.B08301e3]
 //  cols a = B08301e1 , b = B08301e3
 //  NonSOV_e = a - b
-$col_a = "b08301e1";
-$col_b = "b08301e3";
-$calculation = [];
-$data1 = getCol($conn,$col_a,"pm1");
-$data2 = getCol($conn,$col_b,"pm1");
-$arrlength = count($data1);
-echo "<div class='container'>
-        <div class='row'>
-        <div class='col'>";
-// loop through data and insert calculation into array
-// pseudo code: loop(arr[x] = data1[x] - data2[x];)
-echo "NonSOV_e<br>";
-echo "<ol>";
-for($x = 0; $x < $arrlength; $x++) {
-    array_push($calculation,number_format((float)$data1[$x] - $data2[$x],6));
-
-    echo "<li> $calculation[$x]"; // 'echo' for visualization & testing purposes
-}
-$lookup_result["nonsov_e"] = $calculation;
-echo "</ul></div>";
-
-//////////////////////////////////////---End of NonSOV_e--/////////////////////////////////////////////////
-
-//////////////////////////////////////---NonSOV_m begin--/////////////////////////////////////////////////
-//  calculating: NonSOV_m: [X08_COMMUTING.B08301m1] - [X08_COMMUTING.B08301m3]
-//  cols a = B08301m1 , b = B08301m3
-//  NonSOV_m = a - b
-$col_a = "b08301m1";
-$col_b = "b08301m3";
-$calculation = [];
-$data1 = getCol($conn,$col_a,"pm1");
-$data2 = getCol($conn,$col_b,"pm1");
-$arrlength = count($data1);
-
-echo "<div class='col'>";
-// loop through data and insert calculation into array
-// pseudo code: loop(arr[x] = data1[x] - data2[x];)
-echo "NonSOV_m<br>";
-echo "<ol>";
-for($x = 0; $x < $arrlength; $x++) {
-    array_push($calculation,number_format((float)$data1[$x] - $data2[$x],6));
-
-    echo "<li> $calculation[$x]"; // 'echo' for visualization & testing purposes
-}
-$lookup_result["nonsov_m"] = $calculation;
-echo "</ul></div>
-    
-";
+//$col_a = "b08301e1";
+//$col_b = "b08301e3";
+//$calculation = [];
+//$data1 = getCol($conn,$col_a,"pm1");
+//$data2 = getCol($conn,$col_b,"pm1");
+//$arrlength = count($data1);
+//echo "<div class='container'>
+//        <div class='row'>
+//        <div class='col'>";
+//// loop through data and insert calculation into array
+//// pseudo code: loop(arr[x] = data1[x] - data2[x];)
+//echo "NonSOV_e<br>";
+//echo "<ol>";
+//for($x = 0; $x < $arrlength; $x++) {
+//    array_push($calculation,number_format((float)$data1[$x] - $data2[$x],6));
+//
+//    echo "<li> $calculation[$x]"; // 'echo' for visualization & testing purposes
+//}
+//$lookup_result["nonsov_e"] = $calculation;
+//echo "</ul></div>";
+//
+////////////////////////////////////////---End of NonSOV_e--/////////////////////////////////////////////////
+//
+////////////////////////////////////////---NonSOV_m begin--/////////////////////////////////////////////////
+////  calculating: NonSOV_m: [X08_COMMUTING.B08301m1] - [X08_COMMUTING.B08301m3]
+////  cols a = B08301m1 , b = B08301m3
+////  NonSOV_m = a - b
+//$col_a = "b08301m1";
+//$col_b = "b08301m3";
+//$calculation = [];
+//$data1 = getCol($conn,$col_a,"pm1");
+//$data2 = getCol($conn,$col_b,"pm1");
+//$arrlength = count($data1);
+//
+//echo "<div class='col'>";
+//// loop through data and insert calculation into array
+//// pseudo code: loop(arr[x] = data1[x] - data2[x];)
+//echo "NonSOV_m<br>";
+//echo "<ol>";
+//for($x = 0; $x < $arrlength; $x++) {
+//    array_push($calculation,number_format((float)$data1[$x] - $data2[$x],6));
+//
+//    echo "<li> $calculation[$x]"; // 'echo' for visualization & testing purposes
+//}
+//$lookup_result["nonsov_m"] = $calculation;
+//echo "</ul></div>
+//
+//";
 //////////////////////////////////////---End of NonSOV_m--/////////////////////////////////////////////////
 
 
@@ -92,23 +94,23 @@ echo "</ul></div>
 //  cols a = NonSOV_e , b = Ratio_Area
 //  PM_RatioIN_e = a * b
 // col_a not needed
-$col_b = "ratio_area";
-$calculation = [];
-$data1= array_values($lookup_result["nonsov_e"]);
-$data2 = getCol($conn,$col_b,"pm1");
-$arrlength = count($data2);
-echo "<div class='col'>";
-// loop through data and insert calculation into array
-// pseudo code: loop(arr[x] = data1[x] * data2[x];)
-echo "PM_RatioIn_e<br>";
-echo "<ol>";
-for($x = 0; $x < $arrlength; $x++) {
-    array_push($calculation,number_format((float)$data1[$x] * $data2[$x],11));
-
-    echo "<li> $calculation[$x]"; // 'echo' for visualization & testing purposes
-}
-$lookup_result["ratioin_e"] = $calculation;
-echo "</ul></div>";
+//$col_b = "ratio_area";
+//$calculation = [];
+//$data1= array_values($lookup_result["nonsov_e"]);
+//$data2 = getCol($conn,$col_b,"pm1");
+//$arrlength = count($data2);
+//echo "<div class='col'>";
+//// loop through data and insert calculation into array
+//// pseudo code: loop(arr[x] = data1[x] * data2[x];)
+//echo "PM_RatioIn_e<br>";
+//echo "<ol>";
+//for($x = 0; $x < $arrlength; $x++) {
+//    array_push($calculation,number_format((float)$data1[$x] * $data2[$x],11));
+//
+//    echo "<li> $calculation[$x]"; // 'echo' for visualization & testing purposes
+//}
+//$lookup_result["ratioin_e"] = $calculation;
+//echo "</ul></div>";
 //////////////////////////////////////---PM_RatioIN_e END--/////////////////////////////////////////////////
 
 //calculating: PM_RatioIN_m: [tl_2017_48_bg_Clip1.NonSOV_m] * [tl_2017_48_bg_Clip1.Ratio_Area]
@@ -133,17 +135,17 @@ echo "</ul></div>";
  *Precondition:
  * @required (connection != False && colName exists in tableName)
  */
-function getCol($conn,$colName,$tableName){
+function getCol($conn,$tableName){
     $toReturn = [];
-    $query = "SELECT $colName FROM $tableName;";
+    $query = "SELECT * FROM $tableName;";
     $result = mysqli_query($conn, $query); // do the query, store in result
-    $arr_1 = array();
-    while($row = $result->fetch_array()){
-        $arr_1[]= $row;
+//    $arr_1 = array();
+    while($row = mysqli_fetch_assoc($result)){
+        array_push($toReturn,$row);
     }
-    foreach ($arr_1 as $key => $value){
-        array_push($toReturn,$value[$colName]);
-    }
+//    foreach ($arr_1 as $key => $value){
+//        array_push($toReturn,$value[$colName]);
+//    }
     return $toReturn;
 }
 
